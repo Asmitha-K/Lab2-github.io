ec170bf2294fd51ff713f5cd07061f7285cb08f8
Asmitha-K <ka2018@hw.ac.uk>
2021-10-23 11:42:29 +0400

    Update

--- game.js
+++ game.js
@@ -42,6 +42,7 @@ function start(){
     bear = new Bear();
     // Adding an event listener to the keypress event
     document.addEventListener("keydown",moveBear,false);
+    
 }
 
 function moveBear(e){
@@ -167,4 +168,24 @@ function start() {
     makeBees();
 }
 
+function moveBees() {
+    //get speed input field value
+    let speed = document.getElementById("speedBees").value;
+    //move each bee to a random location
+    for (let i = 0; i < bees.length; i++) {
+        let dx = getRandomInt(2 * speed) - speed;
+        let dy = getRandomInt(2 * speed) - speed;
+        bees[i].move(dx, dy);
+    }
+}
+
+function updateBees() { //update loop for the game
+    //move bees randomly
+    moveBees();
+    // use a fixed update period
+    let period = 10; // modify this to control refresh period
+    // update timer for the next move
+    updateTimer = setTimeout('updateBees()', period);
+
+}
 
